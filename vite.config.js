17e9b5879ef0fa3cf95092f0f6db7ab96cfe9415
Vikyath Shetty <vikyathshetty055@gmail.com>
2026-07-16 05:39:59 +0530

    Push with latest updates

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), basicSsl()],
+  server: {
+    allowedHosts: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true
+      },
+      '/ws': {
+        target: 'http://127.0.0.1:8000',
+        ws: true
+      }
+    }
+  }
+})
